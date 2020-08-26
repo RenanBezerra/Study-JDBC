acfdcef8e456dd8d6c283efcfe8548e7682f0991
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-08-26 10:40:01 -0300

    Initial commit

---
+++ src/main/java/com/b3/JDBC/StudyJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.b3.JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJdbcApplication.class, args);
+	}
+
+}
