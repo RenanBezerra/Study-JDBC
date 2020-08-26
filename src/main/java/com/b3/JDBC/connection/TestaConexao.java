f1e75a18eff190fc6394bd495f87bb65c9c28cf2
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-08-26 11:30:00 -0300

    1.6
    Primeira conexão com JDBC

---
+++ src/main/java/com/b3/JDBC/connection/TestaConexao.java
@@ -0,0 +1,18 @@
+package com.b3.JDBC.connection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", ".53524893Renan");
+		System.out.println("oi");
+		connection.close();
+	}
+
+}
