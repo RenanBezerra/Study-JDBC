b129ada58fbf9ac7acb87a6935a8cb65f6da7863
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-08-27 16:37:58 -0300

    5.5
    Criando pool de conexões com c3po

--- src/main/java/com/b3/JDBC/connection/ConnectionFactory.java
+++ src/main/java/com/b3/JDBC/connection/ConnectionFactory.java
@@ -1,14 +1,26 @@
 package com.b3.JDBC.connection;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
-	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-				"root", ".53524893Renan");
+	public DataSource dataSource;
 
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword(".53524893Renan");
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexao() throws SQLException {
+		return this.dataSource.getConnection();
 	}
 }
